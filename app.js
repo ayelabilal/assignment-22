ddb7a3a012e46f325c9d3d5c16ba255e540c1c06
ayelabilal <ayelabilal944@gmail.com>
2024-03-26 23:01:47 +0500

    continue

---
+++ app.js
@@ -0,0 +1,61 @@
+// CHAPTER "Math Mathods"
+// 1 (a)
+var number = 3.45214
+console.log(Math.random(number));
+
+// (b)
+var number = 2.64
+console.log(Math.round(number));
+
+// (c)
+var number = 3.45
+console.log(Math.floor(number));
+
+// (d)
+var number = 5.55
+console.log(Math.ceil(number));
+
+// 2(b)
+var numberNegative = -12.34
+console.log(Math.round(numberNegative));
+
+// (c)
+var floor = -3.67
+console.log(Math.floor(floor));
+
+// (d)
+var number = Math.ceil(-3.767)
+console.log(number);
+
+// 3
+var absolute = 5
+console.log(Math.abs(absolute));
+
+// 4
+var number = prompt("enter number");
+ var dice = (Math.floor(Math.random()*6)+1);
+ console.log(dice);
+
+//  5
+var headUser = prompt("enter your name")
+var tailUser = prompt("enter your name")
+
+var toss = Math.random() *2;
+ 
+var convert = Math.floor(toss);
+
+if(convert === 0 ){
+    console.log(headUser,"win the toss");
+}
+else{
+    console.log(tailUser,"win the toss");
+}
+
+// 6
+
+// 7
+var userInput = prompt("enter your weight in kilograms");
+
+for(var i = 0; i >= 50; i++ ){
+    if (user.Input = [i])
+}
