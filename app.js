890203c2acc3c3b34c59140a35c0a8ee6653588e
ayelabilal <ayelabilal944@gmail.com>
2024-03-30 05:40:32 +0500

    complete

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ var number = prompt("enter number");
  var dice = (Math.floor(Math.random()*6)+1);
  console.log(dice);
 
-//  5
+// //  5
 var headUser = prompt("enter your name")
 var tailUser = prompt("enter your name")
 
@@ -54,8 +54,54 @@ else{
 // 6
 
 // 7
-var userInput = prompt("enter your weight in kilograms");
+// var userInput = prompt("enter your weight in kilograms");
 
-for(var i = 0; i >= 50; i++ ){
-    if (user.Input = [i])
-}
+// for(var i = 0; i >= 50; i++ ){
+//     if (user.Input = [i])
+// }
+
+// chapter 26 (Rounding number)
+
+// 1
+var num = Math.ceil (5.43);
+console.log(num);
+
+// 2
+var origNum = 7.8;
+roundNum = Math.ceil(origNum);
+
+console.log("original num", origNum);
+console.log("rounded up number", roundNum);
+
+// chapter 27 (Random Number)
+
+// 1
+var randomNum = Math.floor(Math.random()*50) +1;
+console.log (randomNum);
+
+// 2
+var num = Math.random();
+console.log(num);
+
+// 3
+var num = Math.floor(Math.random()*6)+1;
+console.log(num);
+
+// 4
+var num = Math.floor(Math.random()*2)+1;
+console.log(num);
+
+// chapter 28,29(converting Strings)
+
+// 1
+var num = "123"
+console.log("convert string to integer",num);
+
+// 2
+
+var num = "123"
+console.log("convert string to integer",num);
+
+// 3
+var float = 9.23
+console.log(float);
